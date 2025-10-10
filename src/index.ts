c45a811795c1a0b6ebc115ebeb435c7e1e5e5743
IsAnAn <3599766025@qq.com>
2025-10-10 10:55:21 +0800

    新增计价规则管理功能，支持保存、加载、删除和清空规则；优化按钮样式和布局；添加JS调试器

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export function PcbPrice(): void {
 	eda.sys_IFrame.openIFrame('/iframe/index.html', 700, 500);
 }
+
+export function Debug(): void {
+	eda.sys_IFrame.openIFrame('iframe/DEBUG/index.html', 500, 500);
+}
+
+// console.log(eda.sys_Storage.setExtensionUserConfig("测试项目一",1234)); 写入一次数据
+// console.log(eda.sys_Storage.getExtensionAllUserConfigs()); 获得所有数据
+
+// console.log(eda.sys_Storage.deleteExtensionUserConfig("测试项目一")); 删除一次数据
+// console.log(eda.sys_Storage.clearExtensionAllUserConfigs()); 删除所有数据
